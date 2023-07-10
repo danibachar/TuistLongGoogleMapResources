7b0ff735d4fcae36398df159ebcbd1760bf37d52
Daniel Bachar <danielbac@zillowgroup.com>
2023-07-10 09:13:18 -0600

    init

---
+++ Targets/TuistLongGoogleMapResourcesKit/Tests/TuistLongGoogleMapResourcesKitTests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistLongGoogleMapResourcesKitTests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistLongGoogleMapResourcesKit", "TuistLongGoogleMapResourcesKit")
+    }
+}
---
+++ Targets/TuistLongGoogleMapResourcesUI/Tests/TuistLongGoogleMapResourcesUITests.swift
@@ -0,0 +1,8 @@
+import Foundation
+import XCTest
+
+final class TuistLongGoogleMapResourcesUITests: XCTestCase {
+    func test_example() {
+        XCTAssertEqual("TuistLongGoogleMapResourcesUI", "TuistLongGoogleMapResourcesUI")
+    }
+}
